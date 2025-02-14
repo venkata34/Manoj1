83c16d424afde15bbfdd8839d5b879b52c9cf54d
manoj <155070871+venkata34@users.noreply.github.com>
2025-02-14 12:25:28 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("yes-btn").addEventListener("click", function() {
+    document.getElementById("response").innerHTML = "Yay! I love you ❤️";
+});
+document.getElementById("no-btn").addEventListener("click", function() {
+    document.getElementById("response").innerHTML = "Oh no! 😢";
+});
